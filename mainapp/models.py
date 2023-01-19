334dd026ecc5bb0f9a04f602d7c8e6804992f495
AntonDubovitskii <96079388+AntonDubovitskii@users.noreply.github.com>
2023-01-19 17:42:45 +0400

    lesson3 - Done (#3)

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,3 +15,26 @@ class LibraryUser(models.Model):
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
+
+
+class Worker(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Project(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    project_name = models.CharField(max_length=64, unique=True, verbose_name="Project name")
+    repo_link = models.CharField(max_length=64, unique=True, verbose_name="Repository link")
+    worker = models.ManyToManyField(Worker)
+
+
+class TODO(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, verbose_name="Task")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    creator = models.OneToOneField(Worker, null=True, on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    active = models.BooleanField(default=True)
