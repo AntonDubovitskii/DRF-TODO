19dbced332a065356610d24e329c661ca90a2b53
AntonDubovitskii <96079388+AntonDubovitskii@users.noreply.github.com>
2023-02-13 18:10:26 +0400

    lesson10 - Done (#10)

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'drf_yasg',
+    'graphene_django',
     #My
     'mainapp',
 ]
@@ -166,5 +167,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
+}
+
+GRAPHENE = {
+    "SCHEMA": "config.schema.schema"
 }
 
